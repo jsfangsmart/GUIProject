6175a0b3a25e2e454292d9296661f0d444472cc0
jsfangsmart <jsfang_2008@163.com>
2020-09-22 15:10:23 +0800

    Initial commit

---
+++ src/CommunicationForm.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+/**
+ * auther smart
+ * create 2020/9/22 下午1:40
+ */
+public class CommunicationForm {
+    private JPanel panel1;
+    private JRadioButton radioButton1;
+    private JRadioButton radioButton2;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JButton button1;
+
+}
